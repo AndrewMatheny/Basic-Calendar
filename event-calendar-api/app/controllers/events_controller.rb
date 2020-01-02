f813c72799508733ee9fa65002ef9939dd754734
AndrewMatheny <github email address>
2020-01-02 15:30:10 -0800

    added backend error handling if id not associated with either event or user

--- event-calendar-api/app/controllers/events_controller.rb
+++ event-calendar-api/app/controllers/events_controller.rb
@@ -1,7 +1,11 @@
 class EventsController < ApplicationController
     def show
         event = Event.find_by(id: params[:id])
-        render json: event
+        if event
+            render json: event
+        else
+            render json: { message: 'No event found with that id'}
+        end
     end
     def index
         events = Event.all
--- event-calendar-api/app/controllers/users_controller.rb
+++ event-calendar-api/app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
     def show
         user = User.find_by(id: params[:id])
+        if user
         render json: user
+        else
+            render json: { message: 'No user found with that id'}
+        end
     end
 
     def index
